6b1281934784786057e5a923d1517b660dc26ece
Timm <timm930@gmail.com>
2014-03-02 02:01:03 +0800

    "婚姻建議" 創建
    創立"Marry Suggestion"這個練習用app

---
+++ MarrySuggestion/src/com/example/marrysuggestion/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.marrysuggestion;
+
+import android.os.Bundle;
+import android.app.Activity;
+//import android.view.Menu;
+import android.view.View;
+import android.widget.*;
+
+public class MainActivity extends Activity {
+
+	private Button btnDoSug;
+    private EditText edtSex, edtAge;
+    private TextView txtResult;
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // 從資源類別R中取得介面元件
+        btnDoSug = (Button)findViewById(R.id.btnDoSug);
+        edtSex = (EditText)findViewById(R.id.edtSex);
+        edtAge = (EditText)findViewById(R.id.edtAge);
+        txtResult = (TextView)findViewById(R.id.txtResult);
+
+   	    // 設定button元件的事件listener
+   	    btnDoSug.setOnClickListener(btnDoSugOnClick);
+    }
+	
+	//listener的設定，button按下後的否應
+	private Button.OnClickListener btnDoSugOnClick = new Button.OnClickListener() {
+		// 按下按鈕後要執行的程式碼
+		public void onClick(View v) {
+			//把性別存到strSex這個字串
+    		String strSex = edtSex.getText().toString();
+    		//把年齡先存到字串，再轉成int
+    		int iAge = Integer.parseInt(edtAge.getText().toString());
+    			
+    		String strSug = "Result：";
+    		if (strSex.equals("male"))
+    			if (iAge < 28)
+    				strSug += "Not hurry";
+    			else if (iAge > 33)
+    				strSug += "Get married";
+    			else
+    				strSug += "Start dating";
+    		else
+    			if (iAge < 25)
+    				strSug += "Not hurry";
+    			else if (iAge > 30)
+    				strSug += "Get married";
+    			else
+    				strSug += "Start dating";
+    		
+    		//把結果存到txtResult這個TextView
+    		txtResult.setText(strSug);
+    	}
+    };
+
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }*/
+    
+}
